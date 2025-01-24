16e97ccc80d8aff167cc444ba4e6f99a81b6d47e
raselsabardila <bautai433@gmail.com>
2025-01-24 21:17:20 +0700

    <fix>(API): ADD MQTT

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const mqtt = require("mqtt")
 require("dotenv").config()
 
 const app = express()
@@ -7,28 +8,65 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 const PORT = process.env.PORT
-let lampp_state = false
 
-app.get(
-    "/api/lampp",
-    (req, res) => {
-        return res.status(200).json({
-            data: lampp_state,
-            status: true
-        })
+const client_id = `server_${ Math.random().toString().slice(3) }`
+const mqtt_url = `mqtt://${ process.env.MQTT_BROKER }:${ process.env.MQTT_PORT }`
+const mqtt_topic = "robomik/data/modules/relay"
+
+const client_mqtt = mqtt.connect(
+    mqtt_url,
+    {
+        clientId: client_id,
+        clean: true,
+        connectTimeout: 4000,
+        username: "",
+        password: "",
+        reconnectPeriod: 1000
     }
 )
 
-app.patch(
+client_mqtt.on("connect", () => {
+    console.log(`Connnected to ${ mqtt_url }`)
+
+    client_mqtt.subscribe([mqtt_topic], () => {
+        console.log(`Subscribe to topic ${ mqtt_topic }`)
+    })
+})
+
+client_mqtt.on("message", (topic, payload) => {
+    console.log(`Receiver message from ${ topic }, message: ${ payload }`)
+})
+
+app.post(
     "/api/lampp",
     (req, res) => {
-        if(req.body.state == null) return res.status(400).json({ status: false, message: "Something Wrong." })
-        
-        lampp_state = req.body.state
+        if(!client_mqtt.connected) return res.status(402).json({ status: false, message: "MQTT not Connected" })
+
+        if(req.body.client_id === undefined || req.body.relay === undefined) return res.status(402).json({
+            status: false,
+            message: "client_id and relay is required."
+        })
+
+        client_mqtt.publish(
+            mqtt_topic,
+            JSON.stringify({
+                "client-id": req.body.client_id,
+                "relay": req.body.relay
+            }),
+            {
+                qos: 0,
+                retain: false
+            },
+            (error) => {
+                if(error) {
+                    console.log(error)
+                }
+            }
+        )
 
         return res.status(200).json({
             status: true,
-            data: lampp_state
+            message: "Success"
         })
     }
 )
